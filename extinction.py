83b8212def97d84938e849a1c5faf44013b18478
Joshua Povick <joshuapovick@joshuas-mbp.msu.montana.edu>
2021-04-30 12:55:26 -0600

    working on age interpoaltion

--- extinction.py
+++ extinction.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.interpolate import InterpolatedUnivariateSpline
+from scipy.stats import binned_statistic
 
 # Effective Wavelengths of different passbands
 leff = {'BP':0.5387,'G':0.6419,'RP':0.7667,'J':1.2345,'H':1.6393,'K':2.1757} # mircons
@@ -72,70 +73,77 @@ def cardelli_b(x):
             b = -3.090+1.825*x+1.206/((x-4.62)**2+0.263)+fb
             return b
     
-def cardelli_alav(x,rv):
+def cardelli_alav(wave,rv):
     '''
     Calculate A\lambda/Av
     
     Inputs:
     ------
-        x: effective wavelength in units of 1/micron
+        wave: effective wavelength in units of micron
         rv: Rv value (=Av/E(B_V))
         
     Output:
     ------
         alav: A\lambda/Av
     '''
+    x=1/wave
     alav = cardelli_a(x)+cardelli_b(x)/rv
     return alav
 
-def cardelli_alebv(x,rv):
+def cardelli_alebv(wave,rv):
     '''
     Calculate relative extinction to E(B-V)
     
     Inputs:
     ------
-        x: effective wavelength in units of 1/micron
+        wave: effective wavelength in units of micron
         rv: Rv value (=Av/E(B_V))
         
     Output:
     ------
         alebv: A\lambda/E(B-V)
     '''
+    x=1/wave
     alebv = cardelli_a(x)*rv+cardelli_b(x)/rv
     return alebv
 
-def cardelli_e12ebv(xblue,xred,rv):
+def cardelli_e12ebv(wave_blue,wave_red,rv):
     '''
     Calculate E(1-2)/E(B-V)
     
     Inputs:
     ------
-        x: effective wavelength in units of 1/micron
+        wave_blue: effective wavelength in units of micron
+        wave_red: effective wavelength in units of micron
         rv: Rv value (=Av/E(B_V))
     
     Output:
     ------
         e12ebv: E(1-2)/E(B-V)
     '''
+    xblue=1/wave_blue
+    xred=1/wave_red
     a2a1rv = (cardelli_a(xblue) - cardelli_a(xred))*rv
     b2b1 = cardelli_b(xblue) - cardelli_b(xred)
     e12ebv = a2a1rv + b2b1
     return e12ebv
     
-def cardelli_e12av(xblue,xred,rv):
+def cardelli_e12av(wave_blue,wave_red,rv):
     '''
     Calculate E(1-2)/Av
     
     Inputs:
     ------
-        xblue: effective wavelength of bluer band in units of 1/micron
-        xred: effective wavelength of redder band in units of 1/micron
+        wave_blue: effective wavelength of bluer band in units of micron
+        wave_red: effective wavelength of redder band in units of micron
         rv: Rv value (=Av/E(B_V))
     
     Output:
     ------
         e12av: E(1-2)/Av
     '''
+    xblue=1/wave_blue
+    xred=1/wave_red
     a2a1 = cardelli_a(xblue) - cardelli_a(xred)
     b2b1rv = (cardelli_b(xblue) - cardelli_b(xred))/rv
     e12av = a2a1 + b2b1rv
@@ -212,3 +220,163 @@ def parsec_teff_2_jk(teff,feh,age,isochrones):
     
     jk = spl(np.log10(teff))
     return jk
+
+def parsec_teff2color(teff,feh,blue,red,age,isochrones):
+    '''
+    Calculate the intrinsic color from a star's given temperture and metallicity using PARSEC isochrones
+    
+    Inputs:
+    ------
+        teff: temperature of star
+        feh: metallicity of star
+        blue: [str] bluer magnitude
+        red: [str] redder magnitude
+        age: age in Gyr to use 
+        isochrones: set of parsec isochrones
+    Output:
+    ------
+        blue_red: expected intrinsic color for the given temperature
+    '''
+    
+    single = isochrones[np.where((isochrones['logAge']==closest(np.log10(age*10**9),isochrones['logAge']))&
+                                 (isochrones['MH']==closest(feh,isochrones['MH'])))]
+    
+    sidx = np.argsort(single['logTe'])
+    slogTe = single['logTe'][sidx]
+    scolor = (single[blue]-single[red])[sidx]
+    
+    spl = InterpolatedUnivariateSpline(slogTe[::2],scolor[::2])
+    
+    blue_red = spl(np.log10(teff))
+    return blue_red
+
+def ccline(x,cer,x0,y0):
+    '''
+    Calculate a line to trace back to the stellar locus in color-color space. The intersection is where the 
+    unreddened star should lie on the stellar locus.
+    
+    Inputs:
+    ------
+        x: values to plug in [1 - 2]
+        cer: color excess ratio [E(3 - 4)/E(1 - 2)]
+        x0: color of some point [1 - 2]_0
+        y0: color of some point [3 - 4]_0
+    '''
+    return np.add(np.multiply(cer,x),np.subtract(y0,np.multiply(cer,x0)))
+
+def parsec_locus_jhjk(x0,y0,cer,feh,age,isochrones):
+    '''
+    Find the where a star would be located on the stellar locus in (J - K)-(J - H) color space.
+    (J - K) is the abscissa and (J - H) is the ordinate.
+    
+    Inputs:
+    ------
+        x0: J - K color of star
+        y0: J - H color of star
+        cer: color excess ratio [E(J - H)/E(J - K)]
+        feh: metallicity of star
+        age: age in Gyr
+        isochrones: set of parsec isochrones
+    
+    Outputs:
+    -------
+        jk_cross: J - K of intersection point
+        jh_cross: J - H of intersection point
+    '''
+    single = isochrones[np.where((isochrones['logAge']==closest(np.log10(age*10**9),isochrones['logAge']))&
+                             (isochrones['MH']==closest(feh,isochrones['MH'])))]
+    xs = (single['Jmag'] - single['Ksmag'])
+    ys = (single['Jmag'] - single['Hmag'])
+    
+    bins = np.arange(np.min(xs),np.max(xs),0.005)
+    binned_color = binned_statistic(xs, ys, statistic='median', bins=bins).statistic
+    
+    fin = np.where((np.isfinite(bins[:-1])==True)&(np.isfinite(binned_color)==True))
+    
+    bins = bins[:-1][fin]
+    binned_color = binned_color[fin]
+    
+    # Interpolate Isochrone
+    spline = InterpolatedUnivariateSpline(bins,binned_color,ext=0)
+    y_spl = spline(bins)
+    
+    # Find the crossing point
+    if y0 - spline(x0)>0:
+        # what to do if star is above isochrone in color-color space
+        return np.squeeze([-9999.0,-9999.0])
+
+    else:
+        # Find roots in difference between the ccline and isochrone
+        func = y_spl - ccline(bins,cer,x0,y0)
+        func_spl = InterpolatedUnivariateSpline(bins,func,ext=0)
+        jk_cross = func_spl.roots()
+        jh_cross = spline(jk_cross)
+        
+        if len(jk_cross)==0:
+            # If cross12 is empty 
+            return np.squeeze([-9999.0,-9999.0])
+        
+        else:
+            # Return Cartesian coordinates of crossing point in color-color space
+            return np.squeeze(list(zip(jk_cross, jh_cross)))
+        
+def parsec_locus(x0,y0,mag1,mag2,mag3,mag4,cer,feh,age,parsec):
+    '''
+    Find the where a star would be located on the stellar locus in (1 - 2)-(3 - 4) color space.
+    (1 - 2) is the abscissa and (3 - 4) is the ordinate.
+    
+    Inputs:
+    ------
+        x0: (1 - 2) color of star off locus
+        y0: (3 - 4) color of star off locus
+        mag1: [str] first magnitude in (1 - 2) color
+        mag2: [str] second magnitude in (1 - 2) color
+        mag3: [str] first magnitude in (3 - 4) color
+        mag4: [str] second magnitude in (3 - 4) color
+        cer: color excess ratio [E(3 - 4)/E(1 - 2)]
+        feh: metallicity of star
+        age: age in Gyr
+        parsec: set of parsec isochrones
+    
+    Outputs:
+    -------
+        cross12: (1 - 2) of intersection point
+        cross34: (3 - 4) of intersection point
+    '''
+    
+    single = parsec[np.where((parsec['logAge']==closest(np.log10(age*10**9),parsec['logAge']))&
+                             (parsec['MH']==closest(feh,parsec['MH'])))]
+    xs = (single[mag1] - single[mag2])
+    ys = (single[mag3] - single[mag4])
+    
+    bins = np.arange(np.min(xs),np.max(xs),0.005)
+    binned_color = binned_statistic(xs, ys, statistic='median', bins=bins).statistic
+    
+    fin = np.where((np.isfinite(bins[:-1])==True)&(np.isfinite(binned_color)==True))
+    
+    bins = bins[:-1][fin]
+    binned_color = binned_color[fin]
+    
+    # Interpolate Isochrone
+    spline = InterpolatedUnivariateSpline(bins,binned_color,ext=0)
+    y_spl = spline(bins)
+    
+    # Find the crossing point
+    if y0 - spline(x0)>0:
+        # what to do if star is above isochrone in color-color space
+        return np.squeeze([-9999.0,-9999.0])
+
+    else:
+        # Find roots in difference between the ccline and isochrone
+        func = y_spl - ccline(bins,cer,x0,y0)
+        func_spl = InterpolatedUnivariateSpline(bins,func,ext=0)
+        cross12 = func_spl.roots()
+        cross34 = spline(cross12)
+        
+        if len(cross12)==0:
+            # If cross12 is empty 
+            return np.squeeze([-9999.0,-9999.0])
+        
+        else:
+            # Return Cartesian coordinates of crossing point in color-color space
+            return np.squeeze(list(zip(cross12, cross34)))
